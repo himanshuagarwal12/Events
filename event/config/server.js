795324ba4709b9d68e892e122b5b4e7fd14f55a6
unknown <himanshu59cool@gmail.com>
2020-09-02 20:42:47 +0530

    gs files

---
+++ event/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'cc32d3c9b15df860414ae556e5d8cd66'),
+    },
+  },
+});
---
+++ event/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ec95ca38-6131-49b8-8f15-123a2459c0ef'
+};
